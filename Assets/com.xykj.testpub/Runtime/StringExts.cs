2076a449acfa05c6455ac7f48c7cf9d0e8f09cb0
lukey <344896145@qq.com>
2025-08-11 09:09:27 +0800

    测试公开库
    测试公开库

---
+++ Assets/com.xykj.testpub/Runtime/StringExts.cs
@@ -0,0 +1,16 @@
+// --- Copyright @ 仙元科技 ---
+using UnityEngine;
+
+namespace com.xykj.testpub
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class StringExts
+	{
+		public static string MyAppent(this string str, string str2)
+		{
+			return str + str2;
+		}
+	}
+}
